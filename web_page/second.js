66cf84080c0cfbddf99d1cbceff0ddc9255b80e0
Thineswar <thineswar06@gmail.com>
2020-12-18 19:42:13 +0800

    Limit upload to PDF files only
    After file upload, the system will check the filetype. If it's a PDF file, nothing happens. If not, an error message pops up and both buttons are disabled.

--- web_page/second.js
+++ web_page/second.js
@@ -62,6 +62,26 @@ $(document).ready(function() {
   if (acc === null){
     $("#upload_button").prop('disabled', true);
   }
+
+  //After file upload, but before any button is pressed
+  $('input').change(function() { 
+    var fileInput = document.getElementById('file_input');   
+    if (fileInput.files.length !== null){
+      var filename = fileInput.files[0].name;
+      var parts = filename.split('.');
+      $('#upload_text').html("<p>" + filename + "</p>");
+      if (parts[parts.length - 1] !== "pdf"){
+        fade_in("alert-danger", "Error!", "Invalid file type. Please upload PDF files only.");
+        $("#upload_button").prop('disabled', true);
+        $("#find_button").prop('disabled', true);
+      }
+      else{
+        $("#message").fadeOut("slow");
+        $("#upload_button").prop('disabled', false);
+        $("#find_button").prop('disabled', false);
+      }
+    } 
+  });
 });
 //hash the input file
 function hash_file(callback) {
@@ -84,13 +104,13 @@ document.getElementById("upload_button").addEventListener("click", function() {
   hash_file(function(err, hash) {
     verify(hash, function(err, resultObj) {
       if (resultObj.block_number > 0) {
-        fade_in("alert-danger", "Error!", " <br> File was already uploaded on <br>" + resultObj.timestamp);
+        fade_in("alert-danger", "Error!", " <br> Certificate was already uploaded on <br>" + resultObj.timestamp);
         console.log("Existing hash found at block #" + resultObj.block_number);
       } else {
         upload_to_blockchain(hash, function(err, tx) {
           if (tx !== null) {
             fade_in("alert-success", "Success!", "Transaction ID: " + tx + ".");
-            console.log("New file upload with hash: " + hash);
+            console.log("New certificate upload with hash: " + hash);
           } else {
             fade_in("alert-danger", "Error!", "Please try again.");
           }
@@ -104,10 +124,10 @@ document.getElementById("find_button").addEventListener("click", function() {
   hash_file(function(err, hash) {
     verify(hash, function(err, resultObj) {
       if (resultObj.block_number > 0) {
-        fade_in("alert-success", "Valid!", " <br> Certificate was issued on " + resultObj.timestamp);
+        fade_in("alert-success", "Valid!", " <br> Certificate was uploaded on <br>" + resultObj.timestamp);
         console.log("Hash found at block #" + resultObj.block_number);
       } else {
-        fade_in("alert-danger", "Invalid!", "Input file cannot be verified.");
+        fade_in("alert-danger", "Invalid!", "Input certificate cannot be verified.");
       }
     });
   });
